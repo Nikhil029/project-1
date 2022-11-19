c569f003127752c109e924d285d6d21c2498f94e
Nikhil <nikhiljha4056@gmail.com>
2022-11-19 12:04:03 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const SliderItems = [
+    {
+        id: 1,
+        img:"https://cdn.stocksnap.io/img-thumbs/960w/shoes-sneakers_9EBTRPN0IA.jpg",
+        title:"BLACK FRIDAY SALE",
+        desc:"BUY THE PERFECT PAIR OF SNEAKERS! AND GET FLAT 30% OFF ON NEXT PURCHASE.",
+        bg:"white",
+    },
+    {
+        id: 2,
+        img:"https://cdn.stocksnap.io/img-thumbs/960w/nike-shoes_0R37EU64ZH.jpg",
+        title:"NEW ARRIVALS",
+        desc:"BUY THE PERFECT PAIR OF SNEAKERS! AND GET FLAT 30% OFF ON NEXT PURCHASE.",
+        bg:"grey",
+    },
+    {
+        id: 3,
+        img:"https://cdn.stocksnap.io/img-thumbs/960w/shoes-sunshine_TBZL54J4MZ.jpg",
+        title:"SPORTS SHOES",
+        desc:"BUY THE PERFECT PAIR OF SNEAKERS! AND GET FLAT 30% OFF ON NEXT PURCHASE.",
+        bg:"light blue",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img:"https://images.unsplash.com/photo-1608231387042-66d1773070a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80",
+        title:"CASUAL SHOES",
+    },
+    {
+        id: 2,
+        img:"https://images.unsplash.com/photo-1533867617858-e7b97e060509?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+        title:"FORMAL SHOES",
+    },
+    {
+        id: 3,
+        img:"https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+        title:"RUNNING SHOES",
+    },
+];
+
+export const popularProducts = [
+    {
+        id: 1,
+        img:"https://images.unsplash.com/photo-1597045566677-8cf032ed6634?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+    {
+        id: 2,
+        img:"https://images.unsplash.com/photo-1600269452121-4f2416e55c28?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+    },
+    {
+        id: 3,
+        img:"https://images.unsplash.com/photo-1608667508764-33cf0726b13a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    },
+    {
+        id: 4,
+        img:"https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    },
+    {
+        id: 5,
+        img:"https://images.unsplash.com/photo-1582588678413-dbf45f4823e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+    },
+    {
+        id: 6,
+        img:"https://images.unsplash.com/photo-1606297255627-c58c609140e9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHJ1bm5pbmclMjBzaG9lc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        id: 7,
+        img:"https://images.unsplash.com/photo-1607330852600-c7f1c8c9eda0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjY3fHxzbmVha2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        id: 8,
+        img:"https://images.unsplash.com/photo-1548369735-f548cbe6a294?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=737&q=80",
+    },
+];
